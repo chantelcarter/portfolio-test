74ba5ce203935760709a0793948d59883175a4a8
chantelcarter <chantel.alexia@gmail.com>
2024-01-19 13:24:11 -0800

    added porjects and contact components

---
+++ chantel-portfolio/src/components/Projects.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Projects = () => {
+  return (
+    <>
+    <div className="projects">
+      <h1>Projects:</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Euismod elementum nisi quis eleifend quam adipiscing vitae proin. At in tellus integer feugiat scelerisque varius. Vitae tempus quam pellentesque nec nam aliquam sem et tortor. Et tortor consequat id porta. Morbi blandit cursus risus at. Mauris ultrices eros in cursus. Velit sed ullamcorper morbi tincidunt ornare massa eget. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Facilisi etiam dignissim diam quis enim lobortis.</p>
+    </div>
+    </>
+  )
+}
+
+export default Projects
